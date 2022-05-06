8cab652fe648ac3417409964238022f736b0f987
dkosters <92864387+dkosters@users.noreply.github.com>
2022-05-06 13:32:16 +0200

    Update cpu_normal.sh
    removed two unnecessary commands.

--- NQS/cpu_normal.sh
+++ NQS/cpu_normal.sh
@@ -24,6 +24,4 @@ export CUDA_VISIBLE_DEVICES=''
 # used to get advanced metrics from ear 
 export SLURM_LOADER_LOAD_NO_MPI_LIB=python
 
-srun --ear=on python $HOME/NQS/rbm.py --alpha $1 --iterations $2 --dtype $3 --device "cpu" &
-
-wait
+srun --ear=on python $HOME/NQS/rbm.py --alpha $1 --iterations $2 --dtype $3 --device "cpu" 
